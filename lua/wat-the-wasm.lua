f68fb9b9f3ce53f77ed03b41f667fbd01ef5af1f
Jakob Getz <jakob@getz.de>
2023-08-22 10:10:18 +0000

    fixed error to buffer bug

--- lua/wat-the-wasm.lua
+++ lua/wat-the-wasm.lua
@@ -14,6 +14,34 @@ local is_wat_file = function()
   return false
 end
 
+local cmd_success = function(cmd)
+  local exit_code = 0
+  local job_id = vim.fn.jobstart(cmd, {
+    on_exit = function(_, code)
+      exit_code = code
+    end
+  })
+  vim.fn.jobwait(job_id, "w")
+  if exit_code == 0 then
+    return true
+  end
+  return false
+end
+
+local execute = function(command)
+  vim.fn.jobstart(command, {
+    on_exit = function(_, code)
+      if code == 0 then
+        local file_path = vim.fn.expand("%:p")
+        original_buffer[file_path] = table.concat(vim.fn.readfile(file_path), "\n")
+        vim.cmd(":%!" .. command)
+        return
+      end
+      vim.cmd(":!" .. command)
+    end
+  })
+end
+
 M.to_wasm = function()
   if not is_wat_file() then
     vim.notify("This command can only be used on .wat files", vim.log.levels.ERROR)
@@ -21,8 +49,8 @@ M.to_wasm = function()
   end
   M.revert()
   local file_path = vim.fn.expand("%:p")
-  original_buffer[file_path] = table.concat(vim.fn.readfile(file_path), "\n")
-  vim.cmd(":%!wat2wasm " .. file_path .. " --output=-")
+  local command = "wat2wasm " .. file_path .. " --output=-"
+  execute(command)
 end
 
 M.revert = function()
@@ -31,6 +59,9 @@ M.revert = function()
     return
   end
   local file_path = vim.fn.expand("%:p")
+  if original_buffer[file_path] == nil then
+    return
+  end
   local current_buffer = vim.api.nvim_get_current_buf()
   vim.api.nvim_buf_set_lines(current_buffer, 0, -1, false, { original_buffer[file_path] })
   original_buffer[file_path] = nil
@@ -43,8 +74,8 @@ M.to_wat = function()
   end
   M.revert()
   local file_path = vim.fn.expand("%:p")
-  original_buffer[file_path] = table.concat(vim.fn.readfile(file_path), "\n")
-  vim.cmd(":%!wat2wasm " .. file_path .. " --output=- | wasm2wat -")
+  local command = "wat2wasm " .. file_path .. " --output=- | wasm2wat -"
+  execute(command)
 end
 
 M.to_wasm_verbose = function()
@@ -54,8 +85,8 @@ M.to_wasm_verbose = function()
   end
   M.revert()
   local file_path = vim.fn.expand("%:p")
-  original_buffer[file_path] = table.concat(vim.fn.readfile(file_path), "\n")
-  vim.cmd(":%!wat2wasm -v " .. file_path .. " --output=-")
+  local command = "wat2wasm -v " .. file_path .. " --output=-"
+  execute(command)
 end
 
 M.setup = function(args)
